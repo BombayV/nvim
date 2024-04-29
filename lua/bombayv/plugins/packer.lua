82c1873dfc366cdc07f61717d1805688ada5b00d
BombayV <OMGmsebastian@gmail.com>
2024-04-29 17:45:53 -0400

    packer changes

--- lua/bombayv/plugins/packer.lua
+++ lua/bombayv/plugins/packer.lua
@@ -1,19 +1,33 @@
 vim.cmd [[packadd packer.nvim]]
 
-return require('packer').startup(function(use)
+local status, packer = pcall(require, "packer")
+
+if not status then
+  print("Packer not found")
+  return
+end
+
+
+
+return packer.startup(function(use)
   -- Packer can manage itself
   use ('wbthomason/packer.nvim')
 
+  -- Colors
+  use({ "catppuccin/nvim", as = "catppuccin" }) 
+  
+  -- Navigation
   use({
 	  'nvim-telescope/telescope.nvim', tag = '0.1.6',
 	  -- or                            , branch = '0.1.x',
 	  requires = { {'nvim-lua/plenary.nvim'} }
   })
 
-  use({ "catppuccin/nvim", as = "catppuccin" })
-
   use({ "nvim-treesitter/nvim-treesitter", {run = ':TSUpdate'} })
   use("nvim-treesitter/playground")
+  use("christoomey/vim-tmux-navigator")
+
+  -- Custom things
   use("nvim-lua/plenary.nvim") -- don't forget to add this one if you don't have it yet!
   use({
     "ThePrimeagen/harpoon",
@@ -23,8 +37,12 @@ return require('packer').startup(function(use)
   use("mbbill/undotree")
   use("tpope/vim-fugitive")
 
+  -- LSP
   use("williamboman/mason.nvim")
   use("williamboman/mason-lspconfig.nvim")
-
   use("neovim/nvim-lspconfig")
+
+  if packer_bootstrap then
+    require("packer").sync()
+  end
 end)
--- lua/bombayv/plugins/palette.lua
+++ lua/bombayv/plugins/palette.lua
@@ -1,5 +1,17 @@
 vim.cmd.colorscheme "catppuccin" 
-require("catppuccin.palettes").get_palette "mocha"
-require("catppuccin").setup({
+local paletteStatus, palette = pcall(require, "catppuccin.palettes")
+if not paletteStatus then
+  print("Palette not found")
+  return
+end
+
+local status, colorscheme = pcall(require, "catppuccin")
+if not status then
+  print("Colorscheme not found")
+  return
+end
+
+palette.get_palette "mocha"
+colorscheme.setup({
 	transparent_background = true,
 })
